35236b4057b4207e72548c0ded8e68af7bf7ab9e
Matthew Pagan <mastapegs01@gmail.com>
2020-10-06 00:26:06 -0400

    programatically create links

--- src/web-components/bottom-bar/bottom-bar.js
+++ src/web-components/bottom-bar/bottom-bar.js
@@ -3,23 +3,47 @@ import CSSTemplate from './BottomBarCSSTemplate'
 
 class BottomBar extends LitElement {
 
+  static get properties() {
+    return {
+      links: { type: Array }
+    }
+  }
+
   static get styles() {
     return CSSTemplate
   }
-  
+
+  constructor() {
+    super()
+    this.links = [
+      {
+        href: '/',
+        linkText: 'Home',
+      },
+      {
+        href: '/about',
+        linkText: 'About',
+      },
+      {
+        href: '/contact',
+        linkText: 'Contact',
+      },
+    ]
+  }
+
   render() {
     return html`
       <div class="spacer"></div>
       <div class="container">
         <ul>
-          <li><a href='/'>Home</a></li>
-          <li><a href='/about'>About</a></li>
-          <li><a href='/contact'>Contact</a></li>
+          ${this.links.map(({ href, linkText }) => html`
+            <li><a href=${href}>${linkText}</a></li>
+          `)}
         </ul>
       </div>
     `
   }
-  
+
 }
 
 customElements.define('bottom-bar', BottomBar)
