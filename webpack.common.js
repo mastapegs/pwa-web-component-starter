394de9c5ca53015a315b0659eb6b4cf233c4f7fc
Matthew Pagan <mastapegs01@gmail.com>
2020-10-07 12:33:40 -0400

    webpack pages

--- webpack.common.js
+++ webpack.common.js
@@ -2,14 +2,25 @@ const path = require('path')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
+const htmlFiles = [
+  {
+    filename: 'index.html',
+  },
+  {
+    filename: 'about.html',
+  },
+  {
+    filename: 'contact.html',
+  },
+]
+
 module.exports = {
   entry: {
     app: './src/index.js',
   },
   plugins: [
-    // new CleanWebpackPlugin(['dist/*']) for < v2 versions of CleanWebpackPlugin
     new CleanWebpackPlugin(),
-    new HtmlWebpackPlugin({}),
+    ...htmlFiles.map(({ filename }) => new HtmlWebpackPlugin({ filename })),
   ],
   output: {
     filename: '[name].bundle.js',
