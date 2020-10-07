71547400a7dd74574b4c472a6a87cc4d7bb32e5b
Matthew Pagan <mastapegs01@gmail.com>
2020-10-07 14:45:26 -0400

    added content to home page

--- src/pages/Home/home-page.js
+++ src/pages/Home/home-page.js
@@ -1,11 +1,40 @@
 import { LitElement, html } from 'lit-element'
 
 class HomePage extends LitElement {
+
+  static get properties() {
+    return {
+      techList: { type: Array },
+      frameworkSize: {type: Number},
+    }
+  }
+
+  constructor() {
+    super()
+    this.frameworkSize = 0;
+    this.techList = [
+      'Web Components',
+      'LitElement',
+      'Webpack',
+      'Vercel',
+    ]
+  }
+
   render() {
     return html`
-      <h1>Home</h1>
+      <h1>Simple and Lightweight Web Component Starter</h1>
+      <p>This web app will eventually become a starter for native javascript spa applications.</p>
+      
+      <h2>Technology Used</h2>
+      <ul>
+        ${this.techList.map(tech => html`<li>${tech}</li>`)}
+      </ul>
+
+      <h2>Current JavaScript footprint</h2>
+      <p>JavaScript size: ${this.frameworkSize}kb</p>
     `
   }
+
 }
 
 customElements.define('home-page', HomePage)
