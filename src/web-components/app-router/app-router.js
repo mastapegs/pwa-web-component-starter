941bd3d8f22577ca89a3bcfd0e806937254a6736
Matthew Pagan <mastapegs01@gmail.com>
2020-10-07 14:06:02 -0400

    minor changes

--- src/web-components/app-router/app-router.js
+++ src/web-components/app-router/app-router.js
@@ -5,14 +5,36 @@ import '../../pages/Contact'
 
 class AppRouter extends LitElement {
 
+  static get properties() {
+    return {
+      view: { type: String },
+    }
+  }
+
+  constructor() {
+    super()
+    this.view = 'Home'
+  }
+
+  getView() {
+    switch (this.view) {
+      case 'Home':
+        return html`<home-page></home-page>`
+      case 'About':
+        return html`<about-page></about-page>`
+      case 'Contact':
+        return html`<contact-page></contact-page>`
+      default:
+        return html``
+    }
+  }
+
   render() {
     return html`
-      <home-page></home-page>
-      <about-page></about-page>
-      <contact-page></contact-page>
+      ${this.getView()}
     `
   }
-  
+
 }
 
 customElements.define('app-router', AppRouter)
