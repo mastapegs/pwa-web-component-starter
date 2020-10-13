cc70ab2813525c36588fac7c7391ee5bb373e159
Matthew Pagan <mastapegs01@gmail.com>
2020-10-13 09:26:24 -0400

    remove favicon-webpack-plugin

--- webpack.common.js
+++ webpack.common.js
@@ -3,7 +3,6 @@ const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const WebpackPwaManifest = require('webpack-pwa-manifest')
 const WorkboxPlugin = require('workbox-webpack-plugin')
-const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 const htmlFiles = [
   {
@@ -25,7 +24,6 @@ module.exports = {
     app: './src/index.js',
   },
   plugins: [
-    new FaviconsWebpackPlugin(path.resolve(__dirname, 'src', 'images', 'lightning.png')),
     new CleanWebpackPlugin(),
     ...htmlFiles.map(({ filename }) => new HtmlWebpackPlugin({
       filename,
